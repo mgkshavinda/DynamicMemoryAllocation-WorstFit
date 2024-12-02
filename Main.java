a18ff4fbba302c8352099da2dcdb4d8a2c5e2641
Kavindu Shavinda <kavindu2015@gmail.com>
2024-12-02 21:51:42 +0530

    Updates in Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,51 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] memoryBlocks = {100, 500, 200, 300, 600}; // Available memory block sizes
-        int[] requests = {212, 417, 112, 426}; // Memory allocation requests
+        Scanner scanner = new Scanner(System.in);
 
+        // Get memory blocks from the usercle
+        System.out.println("Enter the number of memory blocks: ");
+        int blockCount = scanner.nextInt();
+        int[] memoryBlocks = new int[blockCount];
+        System.out.println("Enter the sizes of the memory blocks:");
+
+        for (int i = 0; i < blockCount; i++) {
+            System.out.print("Block " + (i + 1) + ": ");
+            memoryBlocks[i] = scanner.nextInt();
+        }
+
+        // Get memory requests from the user
+        System.out.print("\nEnter the number of memory allocation requests: ");
+        int requestCount = scanner.nextInt();
+        int[] requests = new int[requestCount];
+        System.out.println("Enter the sizes of the allocation requests:");
+
+        for (int i = 0; i < requestCount; i++) {
+            System.out.print("Request " + (i + 1) + ": ");
+            requests[i] = scanner.nextInt();
+        }
+
+        
         WorstFitAllocation worstFitAllocation = new WorstFitAllocation();
-
         int[] allocation = worstFitAllocation.worstFit(memoryBlocks, requests);
 
         // Print Results
         System.out.println("Memory Blocks after Allocation: " + Arrays.toString(memoryBlocks));
         System.out.println("Allocation Results: " + Arrays.toString(allocation));
+
+        // Print detailed allocation summary
+        System.out.println("\nAllocation Details:");
+
+        for (int i = 0; i < requests.length; i++) {
+            if (allocation[i] != -1) {
+                System.out.println("Request " + requests[i] + " allocated to Block " + (allocation[i] + 1));
+            } else {
+                System.out.println("Request " + requests[i] + " could not be allocated.");
+            }
+        }
+
+        scanner.close();
     }
 }
